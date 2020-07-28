e9db8c03f1be49123696a0d10740302899d57dbb
SK54678 <sagar.kharade@foolproof.co.uk>
2020-07-28 17:14:02 +0530

    yeah now i got a bitch

--- Test.js
+++ Test.js
@@ -2,10 +2,5 @@ function test(params) {
   console.log(params);
 }
 // SK here
-<<<<<<< HEAD
 // Hi Goofy here
 // b
-=======
-// Hi Goofy here 
-// Do you want some more
->>>>>>> 9758818ca1d26d39d2399175e39af3155ccb0f35
